1e898467f73ec795d539bef466d711ccdae322f6
Zuleidis Puertas <zpuertasedu@gmail.com>
2023-05-19 16:18:13 -0500

    Prueba añadida

---
+++ Js/pruebadeprueba.js
@@ -0,0 +1,39 @@
+// //hallar cuadrado exacto
+// function devolvercuadrados(arr){
+//     let newArr =[];
+//     for (let i=0; i<arr.length;i++){
+//         let raiz = Math.pow(arr[i],0.5)
+//         console.log(raiz)
+//         if (raiz%1===0){
+//          newArr.push(arr[i]);
+         
+//         }
+//     }
+//     console.log(newArr)
+//   return newArr;
+// }
+
+
+// devolvercuadrados(4,5,100,81,7,11)
+
+//sumar desde los números
+function cadenaDeTexto(str){
+    let newStr = str.split(" ");
+    let numeros ={
+        uno :1, "dos":2, "tres":3, "cuatro":4, "cinco":5, "seis":6, "siete":7, "ocho":8, "nueve":9,
+    }
+    console.log(newStr)
+    let suma =0;
+    for (let i=0; i<newStr.length; i++){
+    
+        if (numeros[newStr[i]]){
+            let valor =numeros[newStr[i]];
+            suma+=valor;
+        }
+      
+    }
+    console.log(suma)
+    return suma;
+}
+
+cadenaDeTexto("uno dos tres uno")
