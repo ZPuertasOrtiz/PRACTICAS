32c3e45bc3b6b64cb1bbcda75944f318f97ca824
Zuleidis Puertas <zpuertasedu@gmail.com>
2023-05-29 17:28:41 -0500

    29/05

--- Js/ejercicios.js
+++ Js/ejercicios.js
@@ -165,27 +165,107 @@
 // console.log(contarLetraA("Algoritmos y programación","A"));
 
 //EJERCICIO 9
+ //entendí mal y puse los primos XD
+// const firstPrimeNumbers = (n)=>{
+//     primeNumbers =[1];
+//     for (let i=2; i<=n; i++){
+//         if (isPrime(i)){
+//             primeNumbers.push(i);
+//         }
+//     }
+//     return primeNumbers;
+// }
 
+// const isPrime = (dividend)=>{
+//    let dividers =1;
+//     for (let i=2; i<=dividend; i++){
+//         if(dividend%i ===0)dividers++;
+//         if (dividers>2) return false;
+//     }
+//     return true;  
+// }
+ 
+// console.log(firstPrimeNumbers(13))
 
+//Este si
+//  const firstOddNumbers =  (n) => {
+//   let counter =1;
+//   let odd =[] 
+//   while (counter<=n){
+//     odd.push(2*counter-1);
+//     counter++;
+//   }
+//   return odd;
+//  }
+
+//  console.log(firstOddNumbers(5))
+
+//EJERCICIO 10
+// function vowelsCounter (str){
+//     return str.match(/[aeiouáéíóú]/ig).length;
+// }
+
+// console.log(vowelsCounter("Inteligencia Artificial"))
+// console.log(vowelsCounter("Lenguaje de programación"))
+
+// //FIN------------------------------------------------------------------------------------------
 
 
 
 //EJERCICIO 11
 
-// const pattern = (n)=>{
-//     let ast ="*";
-//     let space = " ";
+const pattern = (n)=>{
+    let ast ="*";
+    let space = " ";
     
-//     for (let i=1; i<=n;i++ ){ 
-//          console.log(space.repeat(n-i)+ast.repeat(2*i-1))
-//     }
-//     for (let j=n-1; j>0;j--){
-//         console.log(space.repeat(n-j)+ast.repeat(2*j-1))
-//     }  
+    for (let i=1; i<=n;i++ ){ 
+         console.log(space.repeat(n-i)+ast.repeat(2*i-1)+space.repeat(n-i))
+    }
+    for (let j=n-1; j>0;j--){
+        console.log(space.repeat(n-j)+ast.repeat(2*j-1)+space.repeat(n-j))
+    }  
       
-// }
+}
     
-// pattern(2)
+pattern(4)
 
 // //FIN------------------------------------------------------------------------------------------
+
+//EJERCICIO 12
+
+// const multipleOfTen = (arr)=>{
+//     let multiples = [];
+//     arr.forEach(number =>{
+//         if(number%10 === 0) multiples.push(number);
+//     })
+//     return multiples;
+
+// }
+//USANDO FILTER
+// const multipleOfTen2 =(arr)=>{
+//     return arr.filter(number=>number%10===0)
+// }
+
+// console.log(multipleOfTen2([20,5,100,81,7,11]))
+// console.log(multipleOfTen2([100000,1,120,12]))
+
+
+
+//FIN------------------------------------------------------------------------------------------
+
+//EJERCICIO 13
+//  const powOfTwo = (n)=>{
+//     let pows =[];
+//     for(let i=0; i<n; i++){
+//         pows.push(2**i);
+//     }
+//     return pows;
+//  }
+//  console.log(powOfTwo(20))
+
+ //FIN------------------------------------------------------------------------------------------
+
+
+ //EJERCICIO 14
+
 
