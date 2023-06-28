ca446c4eafca7b408ffcc0854fd378594fc91b5f
Zuleidis Puertas <zpuertasedu@gmail.com>
2023-06-28 17:04:34 -0500

    Prueba de python

---
+++ pruebasOPP.js
@@ -0,0 +1,34 @@
+// let person = {
+//     name: ["Zule","Fulanita"], //---> propiedad
+//     age: 115, //---> propiedad
+//     greeting: function(){ //---> método
+//         console.log ("Hola,soy"+ this.name[0]+".")
+//     }
+// };
+
+// console.log(person["name"][0])
+
+// function createNewPerson(name) {
+//     var obj = {};
+//     obj.name = name;
+//     obj.greeting = function() {
+//       alert('Hi! I\'m ' + this.name + '.');
+//     };
+//     return obj;
+//   }
+
+//   console.log(createNewPerson("Zule"))
+
+function Persona(nombre, apellido, edad, genero, intereses) {
+
+    // definiendo de propiedades y métodos
+    this.first = first;
+    this.last = last;
+  //...
+  }
+
+  //instancia
+
+let person1 = new Persona('Bob', 'Smith', 32, 'hombre', ['music', 'skiing']);
+person1.valueOf()
+  
